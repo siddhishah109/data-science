fa9ab5cabad5f362413016ea495cb967c499541f
Siddhi Shah <shahsiddhi777@gmail.com>
2024-03-23 19:57:14 +0530

    Remove unused components and exports

---
+++ index.js
@@ -0,0 +1,13 @@
+import TrendGraph from './src/components/TrendGraph';
+import MultiGraph from './src/components/MultiGraph';
+import LinearTrendChart from './src/components/Trends/LinearTrendChart';
+import MovingAverageTrendChart from './src/components/Trends/MovingAverageTrendChart';
+import ExponentialSmoothingChart from './src/components/Trends/ExponentialSmoothingChart';
+import OutlierIQRScatterChart from './src/Outlier/OutlierIQRScatterChart';
+import OutlierZScoreScatterChart from './src/Outlier/OutlierZScoreScatterChart';
+import OutlierKNNScatterChart from './src/Outlier/OutlierKNNScatterChart';
+import ARIMAForecastChart from './src/components/Forcasting/ARIMAForecastChart';
+import SARIMAForecastChart from './src/components/Forcasting/SARIMAForecastChart';
+
+
+export { TrendGraph , MultiGraph , LinearTrendChart , MovingAverageTrendChart , ExponentialSmoothingChart , OutlierIQRScatterChart , OutlierZScoreScatterChart , OutlierKNNScatterChart , ARIMAForecastChart , SARIMAForecastChart };
